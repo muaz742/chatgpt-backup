8d30f2308d899cc9db877e8d46bf3c9e9a58db08
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2024-07-28 15:35:21 +0300

    Target domain updated.
    The old domain, "chat.openai.com", was returning bad requests. As a result, I have switched to the new domain, "chatgpt.com".

--- backup.js
+++ backup.js
@@ -87,7 +87,7 @@ function downloadJson(data) {
 }
 
 async function loadToken() {
-  const res = await fetch('https://chat.openai.com/api/auth/session');
+  const res = await fetch('https://chatgpt.com/api/auth/session');
 
   if (!res.ok) {
     throw new Error('failed to fetch token');
@@ -99,7 +99,7 @@ async function loadToken() {
 
 async function getConversationIds(token, offset = 0) {
   const res = await fetch(
-    `https://chat.openai.com/backend-api/conversations?offset=${offset}&limit=20`,
+    `https://chatgpt.com/backend-api/conversations?offset=${offset}&limit=20`,
     {
       headers: {
         authorization: `Bearer ${token}`,
@@ -123,7 +123,7 @@ async function fetchConversation(token, id, maxAttempts = 3, attempt = 1) {
   const BACKOFF_MULTIPLIER = 2;
   try {
     const res = await fetch(
-      `https://chat.openai.com/backend-api/conversation/${id}`,
+      `https://chatgpt.com/backend-api/conversation/${id}`,
       {
         headers: {
           authorization: `Bearer ${token}`,
